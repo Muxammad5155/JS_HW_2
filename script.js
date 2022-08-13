65b438938504474db321ec81ae6f6d4b5e5cc018
Muxammad <mbossyuldashev@gmail.com>
2022-08-13 18:54:00 +0300

    index.html

---
+++ script.js
@@ -0,0 +1,55 @@
+var num = +prompt(' Soatni Kiriting')
+
+if (num < 1){
+    alert('Tungi Soat 12')
+}else if( num < 2){
+    alert('Ertalabgi Soat 1')
+}else if( num < 3){
+    alert('Ertalabgi Soat 2')
+}else if( num < 4){
+    alert('Ertalabgi Soat 3')
+}else if( num < 5){
+    alert('Ertalabgi Soat 4')
+}else if( num < 6){
+    alert('Ertalabgi Soat 5')
+}else if( num < 7){
+    alert('Ertalabgi Soat 6')
+}else if( num < 8){
+    alert('Ertalabgi Soat 7')
+}else if( num < 9){
+    alert('Ertalabgi Soat 8')
+}else if( num < 10){
+    alert('Ertalabgi Soat 9')
+}else if( num < 11){
+    alert('Ertalabgi Soat 10')
+}else if( num < 12){
+    alert('Ertalabgi Soat 11')
+}else if( num < 13){
+    alert('Obedgi Soat 12')
+}else if( num < 14){
+    alert('Obedgi Soat 1')
+}else if( num < 15){
+    alert('Obedgi Soat 2')
+}else if( num < 16){
+    alert('Obedgi Soat 3')
+}else if( num < 17){
+    alert('Obedgi Soat 4')
+}else if( num < 18){
+    alert('Kechgi Soat 5')
+}else if( num < 19){
+    alert('Kechgi Soat 6')
+}else if( num < 20){
+    alert('Kechgi Soat 7')
+}else if( num < 21){
+    alert('Kechgi Soat 8')
+}else if( num < 22){
+    alert('Kechgi Soat 9')
+}else if( num < 23){
+    alert('Kechgi Soat 10')
+}else if( num < 24){
+    alert('Kechgi Soat 11')
+}else if( num < 25){
+    alert('Tungi Soat 12')
+}else{
+    alert(' Siz Notogri KIritingiz')
+}
